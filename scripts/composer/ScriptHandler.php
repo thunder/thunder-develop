95982006aa1bdd57f583107c682407eb6a6e01a3
Daniel Bosen <daniel.bosen@burda.com>
2018-02-19 10:04:59 +0100

    merge composer json from distribution

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -4,11 +4,17 @@
  * Contains \DrupalProject\composer\ScriptHandler.
  */
 namespace DrupalProject\composer;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\Package\Package;
+use Composer\Repository\VcsRepository;
 use Composer\Script\Event;
-use Composer\Semver\Comparator;
+use Composer\Repository\Vcs\GitHubDriver;
 use DrupalFinder\DrupalFinder;
+use function GuzzleHttp\Psr7\str;
+use Robo\Task\Archive\Pack;
 use Symfony\Component\Filesystem\Filesystem;
 use Webmozart\PathUtil\Path;
+
 class ScriptHandler {
   public static function createRequiredFiles(Event $event) {
     $fs = new Filesystem();
@@ -50,37 +56,51 @@ class ScriptHandler {
       $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
     }
   }
-  /**
-   * Checks if the installed version of Composer is compatible.
-   *
-   * Composer 1.0.0 and higher consider a `composer install` without having a
-   * lock file present as equal to `composer update`. We do not ship with a lock
-   * file to avoid merge conflicts downstream, meaning that if a project is
-   * installed with an older version of Composer the scaffolding of Drupal will
-   * not be triggered. We check this here instead of in drupal-scaffold to be
-   * able to give immediate feedback to the end user, rather than failing the
-   * installation after going through the lengthy process of compiling and
-   * downloading the Composer dependencies.
-   *
-   * @see https://github.com/composer/composer/pull/5035
-   */
-  public static function checkComposerVersion(Event $event) {
+
+  public static function downLoadDistribution(Event $event) {
     $composer = $event->getComposer();
-    $io = $event->getIO();
-    $version = $composer::VERSION;
-    // The dev-channel of composer uses the git revision as version number,
-    // try to the branch alias instead.
-    if (preg_match('/^[0-9a-f]{40}$/i', $version)) {
-      $version = $composer::BRANCH_ALIAS_VERSION;
+    $extra = $composer->getPackage()->getExtra();
+
+    $package = $extra['thunder-package'];
+    $packageName = explode('/', $package)[1];
+
+    foreach($extra['installer-paths'] as $installerPath => $types) {
+      if(in_array('type:drupal-profile', $types)) {
+        $thunderRoot = str_replace('{$name}', $packageName, $installerPath);
+        self::cloneDistribution($event, $thunderRoot);
+        break;
+      }
     }
-    // If Composer is installed through git we have no easy way to determine if
-    // it is new enough, just display a warning.
-    if ($version === '@package_version@' || $version === '@package_branch_alias_version@') {
-      $io->writeError('<warning>You are running a development version of Composer. If you experience problems, please update Composer to the latest stable version.</warning>');
-    }
-    elseif (Comparator::lessThan($version, '1.0.0')) {
-      $io->writeError('<error>Drupal-project requires Composer version 1.0.0 or higher. Please update your Composer before continuing</error>.');
-      exit(1);
+  }
+
+  /**
+   * @param \Composer\Script\Event $event
+   * @param string $thunderRoot
+   */
+  public static function cloneDistribution(Event $event, $thunderRoot): void {
+    $composer = $event->getComposer();
+    $extra = $composer->getPackage()->getExtra();
+    $package = $extra['thunder-package'];
+
+    $repositories = $composer->getRepositoryManager()->getRepositories();
+    $fs = new Filesystem();
+    // Download distribution, if necessary.
+    if (!$fs->exists($thunderRoot)) {
+      foreach ($repositories as $repository) {
+        if ($repository instanceof VcsRepository) {
+          /** @var \Composer\Repository\Vcs\GitHubDriver $gitDriver */
+          $gitDriver = $repository->getDriver();
+          $composerInformation = $gitDriver->getComposerInformation('develop');
+          if ($composerInformation['name'] === $package) {
+            // TODO: use $composer top actually download the repository.
+
+            $repositoryUrl = $gitDriver->getUrl();
+            exec('git clone ' . $repositoryUrl . ' ' . $thunderRoot);
+            $event->getIO()->write("Downloaded Thunder");
+            break;
+          }
+        }
+      }
     }
   }
 }
