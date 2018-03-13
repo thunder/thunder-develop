49200d1c474d89b439b6a7753a142467ce40bda9
Daniel Bosen <daniel.bosen@burda.com>
2018-03-13 18:37:34 +0100

    use required drush

--- scripts/travis/02-install-thunder.sh
+++ scripts/travis/02-install-thunder.sh
@@ -4,4 +4,5 @@ composer config repositories.thunder git https://github.com/BurdaMagazinOrg/thun
 composer config repositories.admin git https://github.com/BurdaMagazinOrg/theme-thunder-admin.git
 composer config repositories.paragraphs_features git https://github.com/thunder/paragraphs_features.git
 composer install
-/usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush si thunder --root=docroot --db-url=mysql://travis@127.0.0.1/thunder -y
+
+/usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" vendor/bin/drush si thunder --root=docroot --db-url=mysql://travis@127.0.0.1/thunder -y
