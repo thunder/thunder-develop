7a1a5bbed827727e41cbcead9295a329fc615539
Daniel Bosen <daniel.bosen@burda.com>
2018-06-25 14:24:37 +0200

    Add custom list module (#19)
    * Increase composers timeout
    
    * Add custom_list module to local develop packages

--- scripts/travis/02-install-thunder.sh
+++ scripts/travis/02-install-thunder.sh
@@ -3,6 +3,7 @@
 composer config repositories.thunder git https://github.com/BurdaMagazinOrg/thunder-distribution.git
 composer config repositories.admin git https://github.com/BurdaMagazinOrg/theme-thunder-admin.git
 composer config repositories.paragraphs_features git https://github.com/thunder/paragraphs_features.git
+composer config repositories.custom_list git https://github.com/thunder/custom_list.git
 composer install
 
 /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" vendor/bin/drush si thunder --root=docroot --db-url=mysql://travis@127.0.0.1/thunder -y
