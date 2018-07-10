64cd48a308286028d3785f31f007c96b1ee2c176
Daniel Bosen <daniel.bosen@burda.com>
2018-07-10 12:07:09 +0200

    Fix composer config

--- scripts/travis/02-install-thunder.sh
+++ scripts/travis/02-install-thunder.sh
@@ -4,7 +4,7 @@ composer config repositories.thunder git https://github.com/BurdaMagazinOrg/thun
 composer config repositories.admin git https://github.com/BurdaMagazinOrg/theme-thunder-admin.git
 composer config repositories.paragraphs_features git https://github.com/thunder/paragraphs_features.git
 composer config repositories.custom_list git https://github.com/thunder/custom_list.git
-composer config repositories.select2 git https://github.com/thunder/custom_list.git
+composer config repositories.select2 git https://github.com/thunder/select2.git
 composer install
 
 /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" vendor/bin/drush si thunder --root=docroot --db-url=mysql://travis@127.0.0.1/thunder -y
