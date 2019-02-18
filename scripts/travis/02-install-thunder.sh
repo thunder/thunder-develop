0b32c66ef365dfd01b448570efd0216e7a718b0e
Christian Fritsch <chr.fritsch@gmx.net>
2019-02-18 10:47:50 +0100

    Use php7.2

--- scripts/travis/02-install-thunder.sh
+++ scripts/travis/02-install-thunder.sh
@@ -12,4 +12,4 @@ composer install
 composer drupal-scaffold
 
 cd docroot
-/usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" ../vendor/bin/drush si thunder --db-url=mysql://travis@127.0.0.1/thunder -y -vvv
+/usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" ../vendor/bin/drush si thunder --db-url=mysql://travis@127.0.0.1/thunder -y
