07411e2587e92a869e0889deaa54e9cee08d9f90
Christian Fritsch <christian.fritsch@burda.com>
2025-01-10 13:40:10 +0000

    More

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -5,9 +5,6 @@ composer install
 
 # Setup docroot
 DOCROOT=$(composer config extra.drupal-scaffold.locations.web-root)
-echo "Setting up docroot: ${DOCROOT}"
+sudo chmod a+x "$(pwd)"
 sudo rm -rf /var/www/html
 sudo ln -s "$(pwd)/${DOCROOT}" /var/www/html
-
-# Start apache
-apache2ctl restart
